47febb80c97616ba28bc077d8d147b178710b054
Leandro Passos <itsmelepassos@gmail.com>
2024-03-04 13:34:30 -0300

    Build Deleted

--- build/controllers/PingController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ping = void 0;
-const ping = (req, res) => {
-    res.json({ pong: true, admin: true });
-};
-exports.ping = ping;
--- build/utils/getCurrentDate.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCurrentDate = void 0;
-const getCurrentDate = () => Intl.DateTimeFormat('pt-br').format(new Date());
-exports.getCurrentDate = getCurrentDate;
